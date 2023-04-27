9de3f3de14302684f752a28ab312a303efa2beec
Neumann <vcomputadoras@gmail.com>
2023-04-27 00:59:26 -0700

    matched background color with button

--- utilities/Safearea.style.component.js
+++ utilities/Safearea.style.component.js
@@ -2,7 +2,7 @@ import { StyleSheet, Platform } from 'react-native';
 export default StyleSheet.create({
     droidSafeArea: {
         flex: 1,
-        backgroundColor: 'lightblue',
+        backgroundColor: '#F4CE14',
         paddingTop: Platform.OS === 'android' ? 30 : 0
     },
 });
