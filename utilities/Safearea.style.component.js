0e13ece05b68c88bcdb2684631d3455240a356d9
Neumann <vcomputadoras@gmail.com>
2023-04-27 10:53:13 -0700

    nothing, delete?

--- utilities/Safearea.style.component.js
+++ utilities/Safearea.style.component.js
@@ -2,7 +2,7 @@ import { StyleSheet, Platform } from 'react-native';
 export default StyleSheet.create({
     droidSafeArea: {
         flex: 1,
-        backgroundColor: '#F4CE14',
+        // backgroundColor: '#F4CE14',
         paddingTop: Platform.OS === 'android' ? 30 : 0
     },
 });
