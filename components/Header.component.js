5f92429fae35403e71f8b2db9ca6655500dfe6ed
Neumann <vcomputadoras@gmail.com>
2023-05-03 20:24:19 -0700

    Styling commponent

--- components/Header.component.js
+++ components/Header.component.js
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text,Image } from "react-native";
 import UserAvatar from "react-native-user-avatar";
-import Logo from "../assets/Logo.png";
+import logo from "../assets/Logo.png";
 import { getUserData } from "../utilities/database";
 import fetchCredentials from "../utilities/fetch.credentials";
 
 function Header() {
+
+  const [loginData, setLoginData] = useState({});
+
+
   useEffect(() => {
     (async () => {
       try {
-        // console.log(await fetchCredentials());
-        const uData = await getUserData();
-        console.log(uData || 'nothing to show');
+        const uData = await fetchCredentials();
+
+        if(uData.logged){
+          setLoginData({...loginData, firstname: uData.lastname});
+        }
+
+        console.log(uData);
+        // const uData = await getUserData();
+        // console.log(uData || 'nothing to show');
       } catch (error) {
         console.log(error);
       }
@@ -19,8 +29,13 @@ function Header() {
   }, []);
 
   return (
-    <View>
-      <Text style={{ color: "white" }}>Header</Text>
+    <View style={{ width: '80%', flexDirection: 'row', alignItems:'center',justifyContent:'flex-end'}}>
+      <Image source={logo} resizeMode="cover" />
+      <UserAvatar
+        size={64}
+        name={`${loginData.firstname} ${loginData.lastname}`}
+        bgColor="#000"
+      />
     </View>
   );
 }
