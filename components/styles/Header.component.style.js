1ddc614432571d597abe036b85cce615884cb024
Neumann <vcomputadoras@gmail.com>
2023-05-04 01:08:02 -0700

    Creating header commponent

---
+++ components/styles/Header.component.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // backgroundColor:'#fff',
+    padding: 5,
+    // borderTopWidth: 1,
+    // borderRadious: 16,
+    // borderColor: "purple",
+  },
+  avatarButton: {
+    paddingRight: 15,
+  },
+});
